ed85998820bde87a8c6f8cc954d58f57550be5ee
Antonio de Angelis <Antonio.deAngelis@arm.com>
2022-11-22 15:44:32 +0000

    bootutil/crypto: Common header is used only in case of MCUBOOT_USE_MBED_TLS
    The common.h header available in bootutil/crypto contains only accessor macros
    that are relevant when MCUBOOT_USE_MBED_TLS is set so make sure that those
    defines are available only in such case by proper guarding.
    
    Signed-off-by: Antonio de Angelis <Antonio.deAngelis@arm.com>

--- boot/bootutil/include/bootutil/crypto/common.h
+++ boot/bootutil/include/bootutil/crypto/common.h
@@ -1,19 +1,24 @@
 /*
  * SPDX-License-Identifier: Apache-2.0
  *
- * Copyright (c) 2021 Arm Limited
+ * Copyright (c) 2021-2022 Arm Limited
  */
 
 #ifndef __BOOTUTIL_CRYPTO_COMMON_H__
 #define __BOOTUTIL_CRYPTO_COMMON_H__
 
-/* TODO May need to update this in a future 3.x version of Mbed TLS.
- * Extract a member of the mbedtls context structure.
+#include "mcuboot_config/mcuboot_config.h"
+
+#ifdef MCUBOOT_USE_MBED_TLS
+#include "mbedtls/build_info.h"
+/* Note: May need to update this in a future 3.x version of Mbed TLS.
+ * Extract a private member of the mbedtls context structure.
  */
 #if MBEDTLS_VERSION_NUMBER >= 0x03000000
 #define MBEDTLS_CONTEXT_MEMBER(X) MBEDTLS_PRIVATE(X)
 #else
 #define MBEDTLS_CONTEXT_MEMBER(X) X
 #endif
+#endif /* MCUBOOT_USE_MBED_TLS */
 
 #endif /* __BOOTUTIL_CRYPTO_COMMON_H__ */
